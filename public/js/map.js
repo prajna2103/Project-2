1f881372231fa62dea92cf006b05f745b8d27a71
Prajna Bal <prajnabal2006@gmail.com>
2026-02-08 11:15:06 +0530

    Add map initialization and marker placement

---
+++ public/js/map.js
@@ -0,0 +1,16 @@
+mapboxgl.accessToken = mapToken;
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v12',
+    center: listing.geometry.coordinates,
+    zoom: 9
+});
+
+const marker = new mapboxgl.Marker({ color: "red" })
+        .setLngLat(listing.geometry.coordinates)
+        .setPopup(
+            new mapboxgl.Popup({offset: 25}).setHTML(
+                `<h5>${listing.location}</h5><p>Within 100m</p>`
+            )
+        )
+        .addTo(map);
